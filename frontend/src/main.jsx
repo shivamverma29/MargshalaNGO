c1824e29ad73936d36841cab683ec3ad2058a8f6
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-06-15 19:33:50 +0530

    commit before backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -6,15 +6,6 @@ import './index.css'
 import {RouterProvider, createBrowserRouter} from "react-router-dom"
 import Home from '../components/Home/Home.jsx'
 import Sign from '../components/Sign/Sign.jsx'
-import Stories from '../components/ExploreStories/Stories.jsx'
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "../components/Home/Home.jsx";
-import Sign from "../components/Sign/Sign.jsx";
 
 const router = createBrowserRouter([
   {
@@ -23,39 +14,18 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-<<<<<<< HEAD
-        element: <Home/>
-    },
-    {
-        path: "login",
-        element: <Sign/>
-    },
-    {
-      path: "stories",
-      element: <Stories/>
-  },
-    ]
-  }
-])
-=======
+
         element: <Home />,
       },
       {
         path: "/login",
         element: <Sign />,
       },
-      {
-        path: "/success",
-        element: <Success />,
-      },
-      {
-        path: "/categories",
-        element: <Categories />,
-      },
+      
     ],
   },
-]);
->>>>>>> f2a98e2fa20a05c404e6187ba47741ea0ca07c19
+]);        
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
