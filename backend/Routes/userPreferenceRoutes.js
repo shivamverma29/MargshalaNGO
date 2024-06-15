76742dcb6b2868399cacd8ec52ecab949e1ad129
Shivam Verma <91418627+shivamverma29@users.noreply.github.com>
2024-06-15 22:42:25 +0530

    mentor card

--- backend/Routes/userPreferenceRoutes.js
+++ backend/Routes/userPreferenceRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const sendData = require("../Controllers/userPreferenceController.js");
+const {
+  sendData,
+  getData,
+} = require("../Controllers/userPreferenceController.js");
 
 const router = express.Router();
 
 router.post("/sendData", sendData);
-// router.get("/getData", getData);
+router.get("/getData", getData);
 
 module.exports = router;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,15 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {RouterProvider, createBrowserRouter} from "react-router-dom"
-import Home from '../components/Home/Home.jsx'
-import Sign from '../components/Sign/Sign.jsx'
-import Success from '../pages/Success/Success.jsx'
-import Categories from '../pages/Categories/Categories.jsx'
-import Category from '../pages/Category/Category.jsx'
-import Admin from '../pages/Admin/Admin.jsx'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home.jsx";
+import Sign from "../components/Sign/Sign.jsx";
+import Success from "../pages/Success/Success.jsx";
+import Categories from "../pages/Categories/Categories.jsx";
+import Category from "../pages/Category/Category.jsx";
+import Admin from "../pages/Admin/Admin.jsx";
+import MentorRequest from "../components/mentor-request/MentorRequest.jsx";
 
 const router = createBrowserRouter([
   {
@@ -42,10 +41,13 @@ const router = createBrowserRouter([
         path: "/admin",
         element: <Admin />,
       },
+      {
+        path: "/admin/mentor-requests",
+        element: <MentorRequest />,
+      },
     ],
   },
-]);        
-
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
