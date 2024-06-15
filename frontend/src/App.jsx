ff014658a277eb4cbbddca1d171162d6378ca0cd
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-06-15 17:50:57 +0530

    add header and other routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
-        app
-      </h1>
+      <Header/>
+      <Outlet/>
+      <Footer/>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {RouterProvider, createBrowserRouter} from "react-router-dom"
+import Home from '../components/Home/Home.jsx'
+import Sign from '../components/Sign/Sign.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/login",
+        element: <Sign/>
+    },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
