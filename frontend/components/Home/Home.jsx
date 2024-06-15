826d5b5dcfd0e8eaaab24203d6b56fa52cd09af8
Ruchitha_Gowda <ruchithagowda242@gmail.com>
2024-06-16 02:35:21 +0530

    ui changes

--- frontend/components/Home/Home.jsx
+++ frontend/components/Home/Home.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
+import "./Home.css"
+import {NavLink} from "react-router-dom"
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
       <div className='bannerBg'>
         
-        <h1>hello</h1>
+      <div class="branches" >
+            <div class="branch">
+                <section class="sec1 jamu" className='states'>
+                    <NavLink to="/categories">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZpQibIWbtuUjPO1QQPx7Qg-t1th-IwMJyA&s" alt="JamuKashmir" style={{
+                      borderRadius:"99%",
+                      height:200,
+                      width:200
+                    }}/>
+                    </NavLink>
+                    <div className='textchild'>
+                    <h1>Uttarakhand</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consectetur ipsa rem, culpa dolore quaerat.</p>
+                    </div>
+                </section>
+
+            </div>
+
+            <div class="branch">
+            <section class="sec1 jamu" className='states'>
+                    <div className='textchild'>
+                    <h1>Karnataka</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consectetur ipsa rem, culpa dolore quaerat.</p>
+                    </div>
+                    <NavLink to="/categories">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZpQibIWbtuUjPO1QQPx7Qg-t1th-IwMJyA&s" alt="JamuKashmir" style={{
+                      borderRadius:"99%",
+                      height:200,
+                      width:200
+                    }}/>
+                    </NavLink>
+                    
+                </section>
+            </div>
+
+            <div class="branch">
+            <section class="sec1 jamu" className='states'>
+                    
+
+            <NavLink to="/categories">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpZpQibIWbtuUjPO1QQPx7Qg-t1th-IwMJyA&s" alt="JamuKashmir" style={{
+                      borderRadius:"99%",
+                      height:200,
+                      width:200
+                    }}/>
+                    </NavLink>
+                    <div className='textchild'>
+                    <h1>Tamil Nadu</h1>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam consectetur ipsa rem, culpa dolore quaerat.</p>
+                    </div>
+                </section>
+            </div>
+        </div>
+
       </div>
     </div>
   )
