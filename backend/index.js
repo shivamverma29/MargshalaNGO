33eb58c9b73f54af66f1999e8373cc37922aeee7
Shivam Verma <91418627+shivamverma29@users.noreply.github.com>
2024-06-15 19:08:34 +0530

    changes in listen

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/userPreference", userPreferenceRoutes);
 
 app.listen(process.env.PORT || 4000, async () => {
-  console.log("connected to port" + PORT);
+  console.log("connected to port" + process.env.PORT);
   try {
     await mongoose.connect(process.env.MONG_URI);
     console.log("connected to mongodb");
