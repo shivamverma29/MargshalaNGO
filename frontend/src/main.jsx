f2a98e2fa20a05c404e6187ba47741ea0ca07c19
Arya Dubal <aryadubal121@gmail.com>
2024-06-15 18:28:26 +0530

    success and cateogries path added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {RouterProvider, createBrowserRouter} from "react-router-dom"
-import Home from '../components/Home/Home.jsx'
-import Sign from '../components/Sign/Sign.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home/Home.jsx";
+import Sign from "../components/Sign/Sign.jsx";
+import Categories from "../pages/Categories/Categories.jsx";
+import Success from "../pages/Success/Success.jsx";
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <App/>,
-    children:[
+    path: "/",
+    element: <App />,
+    children: [
       {
         path: "/",
-        element: <Home/>
-    },
-    {
+        element: <Home />,
+      },
+      {
         path: "/login",
-        element: <Sign/>
-    },
-    ]
-  }
-])
+        element: <Sign />,
+      },
+      {
+        path: "/success",
+        element: <Success />,
+      },
+      {
+        path: "/categories",
+        element: <Categories />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
