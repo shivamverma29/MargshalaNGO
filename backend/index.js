d45a26e252279a58cd937b015d1d755ef7cfdfdc
ak14092003 <arpitdav4444@gmail.com>
2024-06-15 19:43:12 +0530

    update for cloud

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 const cors = require('cors')
 const app = express();
+const authRoutes = require("./Routes/authRoutes.js");
+const userPreferenceRoutes = require("./Routes/userPreferenceRoutes.js");
 
 app.use(cors())
 app.use(fileUpload({
@@ -16,26 +18,21 @@ const cookieParser = require("cookie-parser");
 
 
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
+
 
 app.use(cookieParser());
 
 app.use("/api", require("./Routes/contentUploadRoute"));
 dotenv.config();
 
-const authRoutes = require("./Routes/authRoutes.js");
-const userPreferenceRoutes = require("./Routes/userPreferenceRoutes.js");
+
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/userPreference", userPreferenceRoutes);
 
 app.listen(process.env.PORT || 4000, async () => {
-  console.log("connected to port" + process.env.PORT);
+  console.log("connected to port" +  process.env.PORT);
   try {
     await mongoose.connect(process.env.MONG_URI);
     console.log("connected to mongodb");
